37ce32bb9edbd5f2dfe8939e49ec158f9adfe5ae
mryunqi <434658198@qq.com>
2026-09-01 01:59:18 +0800

    release: v0.1.0-rc.1 测试版发布流水线

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -363,7 +363,7 @@ pub fn diagnostic_text(config: &PluginConfig) -> String {
 
 #[dynamic_plugin(
     id = "xianchen",
-    version = "0.1.0",
+    version = "0.1.0-rc.1",
     api = "0.6",
     config_schema = "../config.schema.json",
     config_ui = "../config.ui.json",
